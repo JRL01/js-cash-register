52490b1b767983d0059cf962c2d74bc8a2c8c822
JRL01 <problematiclogic@gmail.com>
2024-04-08 08:56:50 +0000

    Startd javascript file

---
+++ script.js
@@ -0,0 +1,6 @@
+let price = 1.87;
+let cid = [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.1], ['QUARTER', 4.25], ['ONE', 90], ['FIVE', 55], ['TEN', 20], ['TWENTY', 60], ['ONE HUNDRED', 100]];
+let denominations = [0.01, 0.05, 0.10, 0.25, 1, 5, 10, 20, 100];
+
+const changeDueText = document.getElementById("change-due");
+const cash = document.getElementById("cash");
