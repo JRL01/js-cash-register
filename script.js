1a1c8cc01e682b1dbbbed032d01fd7d361dedeff
JRL01 <problematiclogic@gmail.com>
2024-04-10 12:35:03 +0000

    setup initial checks function

--- script.js
+++ script.js
@@ -2,5 +2,25 @@ let price = 1.87;
 let cid = [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.1], ['QUARTER', 4.25], ['ONE', 90], ['FIVE', 55], ['TEN', 20], ['TWENTY', 60], ['ONE HUNDRED', 100]];
 let denominations = [0.01, 0.05, 0.10, 0.25, 1, 5, 10, 20, 100];
 
-const changeDueText = document.getElementById("change-due");
 const cash = document.getElementById("cash");
+const change = Number(cash.value) - price;
+const screenText = document.getElementById("cash-register-screen");
+const changeText = document.getElementById("change-due");
+
+const registerCheck = () => {
+    if (Number(cash.value) < price) {
+        changeText.innerText = "Customer does not have enough money to purchase the item";
+    } else if (Number(cash.value) === price) {
+        changeText.innerText = "No change due - customer payed with exact cash";
+    } else {
+        let cidSum = 0;
+        for (let i = cid.length; i > 0; i--) {
+            cidSum += cid[i][1];
+        }
+        if (change > cidSum) {
+            changeText.innerText = "INSUFFICIENT_FUNDS";
+        } else {
+            cashToDrawer();
+        }
+    }
+};
