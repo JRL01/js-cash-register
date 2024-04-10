e066ad0b96ef1dc35aeb223595b52a5474cdd7d9
JRL01 <problematiclogic@gmail.com>
2024-04-10 19:28:46 +0000

    setup functions

--- script.js
+++ script.js
@@ -3,15 +3,25 @@ let cid = [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.1], ['QUARTER', 4.25], 
 let denominations = [0.01, 0.05, 0.10, 0.25, 1, 5, 10, 20, 100];
 
 const cash = document.getElementById("cash");
-const change = Number(cash.value) - price;
 const screenText = document.getElementById("cash-register-screen");
 const changeText = document.getElementById("change-due");
+const change = Number(cash.value) - price;
 
 for (let i = cid.length; i > 0; i--) {
     cidSum += cid[i][1];
 }
 
-const registerCheck = () => {};
+const registerCheck = () => {
+    if (Number(cash.value) < price) {
+        changeText.innerText = "Customer does not have enough money to purchase the item";
+        return;
+    } else if (Number(cash.value) == price) {
+        changeText.innerText = "No change due - customer payed with exact cash";
+        return;
+    } else if (change > cidSum) {
+        changeText.innerText = "INSUFFICIENT_FUNDS";
+    }
+};
 
 const cashToDrawer = () => {};
 
