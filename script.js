0108a08fdbefc1d22b44478ea5006f34565484d5
JRL01 <problematiclogic@gmail.com>
2024-04-10 18:59:37 +0000

    restarted structure

--- script.js
+++ script.js
@@ -7,20 +7,12 @@ const change = Number(cash.value) - price;
 const screenText = document.getElementById("cash-register-screen");
 const changeText = document.getElementById("change-due");
 
-const registerCheck = (cash, change, changeText) => {
-    if (Number(cash.value) < price) {
-        changeText.innerText = "Customer does not have enough money to purchase the item";
-    } else if (Number(cash.value) === price) {
-        changeText.innerText = "No change due - customer payed with exact cash";
-    } else {
-        let cidSum = 0;
-        for (let i = cid.length; i > 0; i--) {
-            cidSum += cid[i][1];
-        }
-        if (change > cidSum) {
-            changeText.innerText = "INSUFFICIENT_FUNDS";
-        } else {
-            cashToDrawer();
-        }
-    }
-};
+for (let i = cid.length; i > 0; i--) {
+    cidSum += cid[i][1];
+}
+
+const registerCheck = () => {};
+
+const cashToDrawer = () => {};
+
+const makeChange = () => {};
